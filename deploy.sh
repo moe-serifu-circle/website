26ca42597a9467e9b435216e7e5b07ea935a873e
Rebecca Nelson <tails.fanatic@gmail.com>
2017-07-22 15:32:23 -0500

    Fixed bad deploy script (hopefully)

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ ERROR_ARGS=4
 
 function error()
 {
-	echo -e "\033[0;31m$1\033[0m" >2
+	echo -e "\033[0;31m$1\033[0m" >&2
 }
 
 function check_permissions()
@@ -64,27 +64,35 @@ function check_permissions()
 }
 
 # get commit message to use
+message_set=
 commit_message=
 if [ $# -ge 1 ]
 then
 	if [ "$1" == '-n' ]
 	then
 		commit_message="$(git log -1 --pretty=%B)"
+		message_set=1
 	elif [ "$1" == '-m' ]
 	then
 		[ $# -ge 2 ] || { error "-m requires a message" ; exit $ERROR_ARGS ; }
 		commit_message="$2"
-	else
-		echo "Message for deploy commit (leave empty to use last commit in source repo):"
-		read commit_message
-		if [ -z "$commit_message" ]
-		then
-			commit_message="$(git log -1 --pretty=%B)"
-		fi
+		message_set=1
 	fi
 fi
+
+if [ -z "$message_set" ]
+then
+	echo "Message for deploy commit (leave empty to use last commit in source repo):"
+	read commit_message
+	if [ -z "$commit_message" ]
+	then
+		commit_message="$(git log -1 --pretty=%B)"
+	fi
+	message_set=1
+fi
+
 # if the deploy repo is not cloned, do it now
-if [ ! -a "$deploy_dir" ]
+if [ ! -e "$deploy_dir" ]
 then
 	echo "Deploy dir is missing; cloning..."
 	git clone "git@github.com:$deploy_repo.git" "$deploy_dir" || { error "could not clone repo: $deploy_repo" ; exit $ERROR_GIT ; }
@@ -101,13 +109,13 @@ check_permissions "$publish_dir" || exit $ERROR_FS
 
 # copy it into the build dir and commit it
 cd "$deploy_dir"
-git fetch . || { error "could not perform fetch on deployment repo" ; exit $ERROR_GIT ; }
+git fetch || { error "could not perform fetch on deployment repo" ; exit $ERROR_GIT ; }
 git checkout "$deploy_branch" || { error "could not checkout branch $deploy_branch" ; exit $ERROR_GIT ; }
 git pull || { error "could not pull branch $deploy_branch" ; exit $ERROR_GIT ; }
 cd ..
 cp -R "$publish_dir"/* "$deploy_dir" || { error "could not copy site into deploy dir" ; exit $ERROR_FS ; }
 cd "$deploy_dir"
-git add . || { error "could not stage new files for commit" ; exit $ERROR_GIT }
+git add . || { error "could not stage new files for commit" ; exit $ERROR_GIT ; }
 git commit -m "$commit_message" || { error "could not create commit" ; exit $ERROR_GIT ; }
 git push origin "$deploy_branch" || { error "could not push commit" ; exit $ERROR_GIT ; }
 cd ..
